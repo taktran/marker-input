071dc078d1ef55fc7183ee08ae92acde1fb5eedf
Tak Tran <contact@tutaktran.com>
2013-03-05 14:24:05 +1100

    Extract student id and mark using regex.

--- mark-docx.rb
+++ mark-docx.rb
@@ -1,7 +1,21 @@
 #!/usr/bin/env ruby
 require 'docx'
 
+student_id_regex = /Student ID: *(\d*)/i
+mark_regex = /Mark: *(\d*)/i
+
 assessment = Docx::Document.open('docs/assessment.docx')
+student_id = nil
+mark = nil
 assessment.each_paragraph do |p|
-  puts p
+  p_string = p.to_s
+
+  student_id_match = student_id_regex.match(p_string)
+  student_id = student_id_match[1] if student_id_match
+
+  mark_match = mark_regex.match(p_string)
+  mark = mark_match[1] if mark_match
 end
+
+puts "Student ID = #{student_id ? student_id : '[not found]'}"
+puts "Mark = #{mark ? mark : '[not found]'}"
